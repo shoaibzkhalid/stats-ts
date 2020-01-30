6eca922ee8414da468bede7676dbc66fab6f2f4d
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2020-01-30 13:24:08 +0500

    deleted build directory

--- build/MatchResults.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MatchResults;
-(function (MatchResults) {
-    MatchResults["HomeWin"] = "H";
-    MatchResults["AwayWin"] = "A";
-    MatchResults["Draw"] = "D";
-})(MatchResults = exports.MatchResults || (exports.MatchResults = {}));
